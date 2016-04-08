5a2ab3796036815b842723a147387f81dc9e7a02
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-04-08 23:47:09 +0200

    Updates to work with latest Core changes

--- src/main/java/com/almightyalpaca/discord/bot/plugin/stats/StatsPlugin.java
+++ src/main/java/com/almightyalpaca/discord/bot/plugin/stats/StatsPlugin.java
@@ -1,8 +1,8 @@
 package com.almightyalpaca.discord.bot.plugin.stats;
 
-import com.almightyalpaca.discord.bot.system.command.AbstractCommand;
-import com.almightyalpaca.discord.bot.system.command.annotation.Command;
-import com.almightyalpaca.discord.bot.system.events.CommandEvent;
+import com.almightyalpaca.discord.bot.system.command.Command;
+import com.almightyalpaca.discord.bot.system.command.CommandHandler;
+import com.almightyalpaca.discord.bot.system.events.commands.CommandEvent;
 import com.almightyalpaca.discord.bot.system.exception.PluginLoadingException;
 import com.almightyalpaca.discord.bot.system.exception.PluginUnloadingException;
 import com.almightyalpaca.discord.bot.system.plugins.Plugin;
@@ -14,13 +14,13 @@ import net.dv8tion.jda.entities.Guild;
 
 public class StatsPlugin extends Plugin {
 
-	class StatsCommand extends AbstractCommand {
+	class StatsCommand extends Command {
 
 		public StatsCommand() {
 			super("stats", "Only some stats...", "");
 		}
 
-		@Command(dm = true, guild = true, async = true)
+		@CommandHandler(dm = true, guild = true, async = true)
 		private void onCommand(final CommandEvent event) {
 			final MessageBuilder builder = new MessageBuilder();
 
@@ -37,22 +37,22 @@ public class StatsPlugin extends Plugin {
 			builder.appendString("" + channels, Formatting.BOLD).appendString(" channels and ");
 			builder.appendString("" + users, Formatting.BOLD).appendString(" known users.");
 
-//			builder.newLine().newLine();
-//
-//			MemoryMXBean bean = ManagementFactory.getMemoryMXBean();
-//
-//			long used = bean.getHeapMemoryUsage().getUsed() + bean.getNonHeapMemoryUsage().getUsed();
-//			long max = bean.getHeapMemoryUsage().getMax() + bean.getNonHeapMemoryUsage().getMax();
-//			long free = max - used;
-//			long total = Runtime.getRuntime().maxMemory();
-//
-//			String memory = "";
-//			memory += "Used:   " + toMB(used) + "MB\n";
-//			memory += "Free:   " + toMB(free) + "MB\n";
-//			memory += "Total:  " + toMB(total) + "MB\n";
-//			memory += "Max:    " + toMB(max) + "MB\n";
-//
-//			builder.appendString("Memory Usage:", Formatting.BOLD).newLine().appendCodeBlock(memory, "");
+			// builder.newLine().newLine();
+			// 
+			// MemoryMXBean bean = ManagementFactory.getMemoryMXBean();
+			// 
+			// long used = bean.getHeapMemoryUsage().getUsed() + bean.getNonHeapMemoryUsage().getUsed();
+			// long max = bean.getHeapMemoryUsage().getMax() + bean.getNonHeapMemoryUsage().getMax();
+			// long free = max - used;
+			// long total = Runtime.getRuntime().maxMemory();
+			// 
+			// String memory = "";
+			// memory += "Used:   " + toMB(used) + "MB\n";
+			// memory += "Free:   " + toMB(free) + "MB\n";
+			// memory += "Total:  " + toMB(total) + "MB\n";
+			// memory += "Max:	" + toMB(max) + "MB\n";
+			// 
+			// builder.appendString("Memory Usage:", Formatting.BOLD).newLine().appendCodeBlock(memory, "");
 
 			event.sendMessage(builder.build());
 
